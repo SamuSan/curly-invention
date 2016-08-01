7a116024b89957252589c009c9e220f72c2ec1c5
Sam Minns <sam.minns@powershop.co.nz>
2016-08-02 10:39:22 +1200

    Ruby version updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'performance/index'
-  get 'performance/show'
-  get 'performance/stop_performance'
+  root 'performance#index'
+  get  'performance/index'
+  get  'performance/show'
+  get  'performance/stop_performance'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'performance#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
